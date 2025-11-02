07fc5a889ab85c1f44da06d1c98648f54c0ba6e3
Rtwotwo <yoloredal@gmail.com>
2025-11-02 22:51:28 +0800

    finish system_utils.py of utils

---
+++ scene/dataset_readers.py
@@ -0,0 +1,6 @@
+"""
+Author: Redal
+Date: 2025-11-02
+Todo: Build dataset reader for __init__.py to load scene data
+Homepage: https://github.com/Rtwotwo/3DRepo
+"""
--- utils/system_utils.py
+++ utils/system_utils.py
@@ -10,4 +10,14 @@ from os import makedirs, path
 
 
 def mkdir_p(folder_path: str)->None:
-    
+    """创建一个目录,相当于在命令行中使用mkdir -p"""
+    try: makedirs(folder_path)
+    except OSError as exc:
+        if exc.errno==EEXIST and path.isdir(folder_path): pass
+        else: raise
+
+
+def searchForMaxIteration(folder:str)->int:
+    """查找指定文件夹中文件名所包含的最大迭代次数"""
+    saved_iters = [int(fname.split('_')[-1]) for fname in os.listdir(folder)]
+    return max(saved_iters)
