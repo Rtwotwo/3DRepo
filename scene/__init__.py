16d345d1819f4d5bbf9affb5f959b1c853d1dec2
Rtwotwo <yoloredal@gmail.com>
2025-11-02 20:19:57 +0800

    Learn to build scene and utils foler

--- scene/__init__.py
+++ scene/__init__.py
@@ -1,9 +1,12 @@
 """
 Author: Redal
-Date: 2025-10-29
-Todo: Make Mip-Splatting Model 
-Homepage: https://github.com/Rtwotwo/3DRepo.git
+Date: 2025-11-02
+Todo: Learn how to build scene folder from GRAPHDECO 
+      research group https://team.inria.fr/graphdeco
+Homepage: https://github.com/Rtwotwo/3DRepo
 """
 import os
 import random
 import json
+from utils.system_utils import searchForMaxIteration
+from scene.dataset_readers import sceneLoadTypeCallbacks
---
+++ utils/system_utils.py
@@ -0,0 +1,13 @@
+"""
+Author: Redal
+Date: 2025-11-02
+Todo: 
+Homepage: https://github.com/Rtwotwo/3DRepo
+"""
+import os
+from errno import EEXIST
+from os import makedirs, path
+
+
+def mkdir_p(folder_path: str)->None:
+    
